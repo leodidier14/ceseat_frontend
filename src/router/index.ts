c796230da59e8823901fff03351c8171b0e38d4c
dylanLafarge <dylan.lafarge@viacesi.fr>
2021-06-19 19:18:02 +0200

    First commit

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,22 +1,16 @@
 import Vue from 'vue'
 import VueRouter, { RouteConfig } from 'vue-router'
-import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
 
 const routes: Array<RouteConfig> = [
   {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
+    path: '/card',
+    name: 'Article-Choice',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    component: () => import(/* webpackChunkName: "about" */ '../views/ArticleChoice.vue')
   }
 ]
 
---
+++ src/views/ArticleChoice.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>Test</div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+
+@Component
+export default class ArticleChoice extends Vue {
+  
+}
+</script>
+
+<style scoped>
+
+</style>
