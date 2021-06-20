158da07a457016e7478adb7788ce8f3613384500
dylanLafarge <dylan.lafarge@viacesi.fr>
2021-06-20 21:36:58 +0200

    Fix negative count.

--- src/components/ArticleCard.vue
+++ src/components/ArticleCard.vue
@@ -7,23 +7,48 @@
         <v-card-subtitle v-text="article.description"></v-card-subtitle>
 
         <v-card-actions>
-          <span class="ml-2 mt-5" v-text="Number.parseFloat(article.price).toFixed(2) + ' €'"></span>
-          <v-btn class="ml-10 mt-5" fab icon height="30px" width="30px" @click="count -= 1">
+          <span
+            class="ml-2 mt-5"
+            v-text="Number.parseFloat(article.price).toFixed(2) + ' €'"
+          ></span>
+          <v-btn
+            class="ml-10 mt-5"
+            fab
+            icon
+            height="30px"
+            width="30px"
+            :disabled="count == 1"
+            @click="count -= 1"
+          >
             <v-icon>mdi-minus</v-icon>
           </v-btn>
           <span class="ml-2 mt-5" v-text="count"></span>
-          <v-btn class="ml-2 mt-5" fab icon height="30px" width="30px" @click="count += 1">
+          <v-btn
+            class="ml-2 mt-5"
+            fab
+            icon
+            height="30px"
+            width="30px"
+            @click="count += 1"
+          >
             <v-icon>mdi-plus</v-icon>
           </v-btn>
 
-          <v-btn class="ml-2 mt-5" color="#CA6B3E" rounded small dark @click="addtoCart()">
+          <v-btn
+            class="ml-2 mt-5"
+            color="#CA6B3E"
+            rounded
+            small
+            dark
+            @click="addtoCart()"
+          >
             Ajouter
           </v-btn>
         </v-card-actions>
       </div>
 
       <v-avatar class="ma-3" size="125" tile>
-          <slot name="article-image">Image de l'article</slot>
+        <slot name="article-image">Image de l'article</slot>
       </v-avatar>
     </div>
   </v-card>
@@ -42,6 +67,16 @@ export default class ArticleCard extends Vue {
   addtoCart() {
     this.$root.$emit("add-to-cart", this.count);
     this.count = 1;
+  }
+
+  subtractCount() {
+    if (this.count > 1) {
+      this.count -= 1;
+    }
+  }
+
+  addCount() {
+    this.count += 1;
   }
 }
 </script>
