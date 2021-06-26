dd1bae357d1608347c59f4c8e4f66037f76d2a47
Leo Didier <leo@sowiso.com>
2021-06-26 11:27:44 +0200

    list component and add form

--- src/views/TechnicianComponents.vue
+++ src/views/TechnicianComponents.vue
@@ -1,10 +1,88 @@
 <template>
   <v-card class="mx-auto" elevation="10" width="80%" height="100%">
-    <h3 class="text-center pt-5" style="font-weight: normal">
+    <h3 class="text-center py-5" style="font-weight: normal">
       Composants disponibles
     </h3>
     <div style="display: flex; justify-content: flex-end">
-      <v-btn small rounded color="#CA6B3E" style="color: white">Ajouter</v-btn>
+      <v-dialog v-model="dialog" width="500">
+        <template v-slot:activator="{ on, attrs }">
+          <v-btn
+            class="mr-3"
+            small
+            rounded
+            color="#CA6B3E"
+            style="color: white"
+            v-bind="attrs"
+            v-on="on"
+          >
+            Ajouter
+          </v-btn>
+        </template>
+
+        <v-card>
+          <v-card-title class="text-h5"> Ajoutez un composant </v-card-title>
+
+          <v-card-text>
+            <v-form v-model="valid" ref="componentForm">
+              <v-container>
+                <v-row>
+                  <v-col cols="12">
+                    <v-text-field
+                      v-model="component.name"
+                      label="Nom"
+                      :rules="[rules.required]"
+                      required
+                    ></v-text-field>
+                  </v-col>
+
+                  <v-col cols="12">
+                    <v-textarea
+                      v-model="component.description"
+                      color="teal"
+                      :rules="[rules.required]"
+                    >
+                      <template v-slot:label>
+                        <div>Description</div>
+                      </template>
+                    </v-textarea>
+                  </v-col>
+
+                  <v-col cols="12">
+                    <v-text-field
+                      v-model="component.version"
+                      label="Version"
+                      :rules="[rules.required]"
+                      required
+                    ></v-text-field>
+                  </v-col>
+                  <v-col cols="12">
+                    <v-text-field
+                      v-model="component.documentationLink"
+                      label="Lien documentation"
+                      :rules="[rules.required]"
+                      required
+                    ></v-text-field>
+                  </v-col>
+                  <v-col cols="12">
+                    <v-text-field
+                      v-model="component.downloadLink"
+                      label="Lien téléchargement"
+                      :rules="[rules.required]"
+                      required
+                    ></v-text-field>
+                  </v-col>
+                </v-row>
+              </v-container> </v-form
+          ></v-card-text>
+
+          <v-divider></v-divider>
+
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn color="#CA6B3E" text @click="submitForm()">Ajouter</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
     </div>
 
     <v-data-table
@@ -62,9 +140,12 @@
 <script lang="ts">
 import { Component, Prop, Vue } from "vue-property-decorator";
 import { Components } from "@/shims-tsx";
+import axios from "axios";
 
 @Component
 export default class TechnicianComponents extends Vue {
+  private dialog: boolean = false;
+  private valid: boolean = true;
   private headers: object = [
     {
       text: "Nom",
@@ -88,7 +169,19 @@ export default class TechnicianComponents extends Vue {
       value: "downloadLink",
       sortable: false,
     },
+    {
+      text: "Supprimer",
+      sortable: false,
+    },
   ];
+
+  private component: Components.component = {
+    name: "",
+    version: "",
+    description: "",
+    documentationLink: "",
+    downloadLink: "",
+  };
 
   private components: Array<Components.component> = [
     {
@@ -107,10 +200,41 @@ export default class TechnicianComponents extends Vue {
     },
   ];
 
+  private rules = {
+    required: (value: string) => !!value || "Ce champ est obligatoire.",
+  };
+
   public deleteComponent(item: Components.component) {
     const index = this.components.indexOf(item);
     this.components.splice(index, 1);
     //axios delete
+  }
+
+  public submitForm() {
+    if (
+      (this.$refs.componentForm as Vue & { validate: () => boolean }).validate()
+    ) {
+      this.components.push(JSON.parse(JSON.stringify(this.component)));
+      this.component = {
+        name: "",
+        version: "",
+        description: "",
+        documentationLink: "",
+        downloadLink: "",
+      };
+      this.dialog = false;
+      axios
+        .post("/addcomponent", this.component)
+        .then((res: any) => {
+          //Perform Success Action
+        })
+        .catch((error: any) => {
+          // error.response.status Check status code
+        })
+        .finally(() => {
+          //Perform action in always
+        });
+    }
   }
 }
 </script>
